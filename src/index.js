f2863261a90742841695eb09ade2f7ce022dd590
Marcos Lima <socram.oluap2003@gmail.com>
2022-12-18 00:50:54 -0300

    refatorado funções para logica do jogo de um player, transformando em classe e criando responsivo

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
 import chooseModeGame from "./home.js";
+import GameOnePlayer from "./onePlayer.js";
+
 chooseModeGame();
+
+const gameOnePlayerNormal = new GameOnePlayer('.normal-1 .game-option', 
+'.normal-1 .choose-move','.normal-1 .option-picked', '.normal-1 .score-number', 'normal');
+gameOnePlayerNormal.init();
+
+const gameOnePlayerBonus = new GameOnePlayer('.bonus-1 .game-option', 
+'.bonus-1 .choose-move','.bonus-1 .option-picked', '.bonus-1 .score-number', 'bonus');
+gameOnePlayerBonus.init();
